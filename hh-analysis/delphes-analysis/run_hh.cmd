5c5b862e223afd1f30e5ecbad49da3ae3f5a4efe
alasfar-lina <lina.alasfar@outlook.fr>
2020-12-14 08:12:07 +0100

    delphes analysis phase I

---
+++ hh-analysis/delphes-analysis/run_hh.cmd
@@ -0,0 +1,21 @@
+Beams:frameType = 4
+Beams:LHEF /users/pep/alasfarl/HH-Machine-learning/hh-analysis/generate-events/SMHH-14.lhe
+///////////
+Main:numberOfEvents = 150000
+310:mayDecay = off  //  K0s
+3112:mayDecay = off //  Sigma-
+3122:mayDecay = off //  Lambda
+3222:mayDecay = off //  Sigma+
+3312:mayDecay = off //  Xi-
+3322:mayDecay = off //  Xi0
+3334:mayDecay = off //  Omega-
+54:isResonance = true
+54:m0 = 125.0
+25:onMode=off
+54:onMode=off
+25:onIfMatch = 22 22
+54:onIfMatch = 5 -5
+25:mWidth = 1.
+25:doForceWidth = on
+54:mWidth = 1.
+54:doForceWidth = on
